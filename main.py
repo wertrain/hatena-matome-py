c53fbf13586a64fbfe6ae5c3c716ca03c4fb5f01
wertrain <wertrain@gmail.com>
2016-03-19 21:23:22 +0900

    WIP

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+# -*- coding: utf-8 -*-
+import logging
+from api import apis
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.register_blueprint(apis)
 app.config['DEBUG'] = True
 
-@app.route('/')
-def hello():
-    return render_template('home.html', page_type=0)
+logging.getLogger().setLevel(logging.DEBUG)
 
+@app.route('/')
+def home():
+    return render_template('home.html', page_type=0)
 
 @app.errorhandler(404)
 def page_not_found(e):
