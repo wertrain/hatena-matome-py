1cfa4a2d93fb401752e4ef0a353e2a177c775998
wertrain <wertrain@gmail.com>
2016-03-23 00:18:54 +0900

    WIP

--- api.py
+++ api.py
@@ -4,10 +4,22 @@ import logging
 import urllib
 from google.appengine.api import memcache
 from my.util import hatena
-
+from my.db import datastore
+from datetime import datetime
 from flask import Blueprint
 apis = Blueprint('apis', __name__)
 
 @apis.route('/system/hotentry')
 def hotentry():
-    return hatena.get_hotentry()
+    hotentries = json.loads(hatena.fetch_hotentry())
+    for entry in hotentries['responseData']['feed']['entries']:
+        datastore.add_private_entry({
+            'title': entry['title'],
+            'url': entry['link'],
+            'author': entry['author'],
+            'content': entry['content'],
+            'content_snippet': entry['contentSnippet'],
+            'published_date': datetime.strptime(entry['publishedDate'], '%a, %d %b %Y %H:%M:%S -0700'),
+            'categories': []
+        })
+    return 'add entries.'
--- my/db/datastore.py
+++ my/db/datastore.py
@@ -6,3 +6,30 @@ u"""
 """
 import json
 from google.appengine.ext import db
+
+class PrivateEntry (db.Model):
+    u"""
+        公開前のエントリーを表すデータ
+    """
+    title = db.StringProperty()
+    url = db.StringProperty()
+    author = db.StringProperty()
+    published_date = db.DateTimeProperty(auto_now_add=False)
+    content = db.TextProperty()
+    content_snippet = db.TextProperty()
+    categories = db.StringListProperty(default=[])
+
+def add_private_entry(param):
+    entry = db.Query(PrivateEntry).filter('url =', param.get('url')).get()
+    if entry is not None:
+        return entry
+    entry = PrivateEntry()
+    entry.title = param.get('title')
+    entry.url = param.get('url')
+    entry.author = param.get('author')
+    entry.published_date = param.get('published_date')
+    entry.content = param.get('content')
+    entry.content_snippet = param.get('content_snippet')
+    entry.categories = param.get('categories')
+    entry.put()
+    return entry
